afaa278ed9f8dc5c22b1e6be509a954b68dff37a
Darshan Patel <148180217+darshan9088@users.noreply.github.com>
2024-12-13 04:57:03 +0000

    conversion !

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,14 @@
+let score = "33"
+console.log(typeof score)
+
+valueNumber = Number(score)
+console.log(typeof valueNumber)
+
+let scoree = "abc"
+console.log(typeof scoree)
+
+valueNumber = Number(scoree)
+console.log(typeof (valueNumber))
+
+isLogged = ""
+console.log(typeof isLoggedIn);
