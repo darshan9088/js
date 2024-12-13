3069bb30268a29d75b2788e86dd3038fc8edfc75
Darshan Patel <148180217+darshan9088@users.noreply.github.com>
2024-12-13 05:18:21 +0000

    conversion!

--- 01_basics/03_conversion.js
+++ 01_basics/03_conversion.js
@@ -1,14 +1,28 @@
-let score = "33"
-console.log(typeof score)
+// let score = "33"
+// console.log(typeof score)
 
-valueNumber = Number(score)
-console.log(typeof valueNumber)
+// valueNumber = Number(score)
+// console.log(typeof valueNumber)
 
-let scoree = "abc"
-console.log(typeof scoree)
+// let scoree = "abc"
+// console.log(typeof scoree)
 
-valueNumber = Number(scoree)
-console.log(typeof (valueNumber))
+// valueNumber = Number(scoree)
+// console.log(typeof (valueNumber))
 
-isLogged = ""
-console.log(typeof isLoggedIn);
+// isLogged = ""
+// console.log(typeof isLoggedIn);
+
+let gameCounter = 100 
+// gameCounter++
+++gameCounter
+
+// console.log(gameCounter);
+
+// let x = 3
+// let y = x++
+// console.log(`x:${x} & y:${y}`);
+
+let x = 3
+let y = ++x
+console.log(`x:${x} & y:${y}`);
