ac9f7e05db8b18138ba250d21f5ba4ca2059a40b
Darshan Patel <148180217+darshan9088@users.noreply.github.com>
2024-12-13 15:36:25 +0000

    string operations!

---
+++ 01_basics/05_string.js
@@ -0,0 +1,42 @@
+let name = "darshan"
+let repoCount = 5
+// console.log(name + repoCount + " value"); not to be used as modern code!
+console.log(`Hello! My name is ${name} & I have Total ${repoCount} Repo `);
+
+const gameName = new String('God-Of-War')
+// console.log(gameName[1]);
+// console.log(gameName);
+// console.log(gameName.__proto__)
+
+// console.log(gameName.toLowerCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('G'));
+
+// console.log(gameName.substring(0,4));
+
+// console.log(gameName.slice(-8,4));
+
+const anotherName = "    darshan   "
+// console.log(anotherName);
+// console.log(anotherName.trim());
+
+const url = "https://Darshan.com/darshan%20patel"
+// console.log(url.replace('%20','_'));
+console.log(url.replace('patel','daslaniya'));
+
+console.log(url.includes('Darshan'));
+console.log(url.includes('sundar'));
+
+console.log(gameName.split('-'));
+
+
+
+
+
+
+
+
+
+
+
+
